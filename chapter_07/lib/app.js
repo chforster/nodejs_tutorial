5d042593bd8b29f765fd05e3cb6691c6a836c007
Christoph Forster <christoph@forster.tirol>
2016-06-13 10:03:17 +0200

    app now takes an argument to specify an input

--- chapter_07/lib/app.js
+++ chapter_07/lib/app.js
@@ -1,9 +1,20 @@
-var input = 'Hello World\nHow are  you?'
+var readline = require('readline');
+var fs = require('fs');
 
-var chars = input.length;
-var words = input.split(/[\t \n]/).filter(e => e !== '').length;
-var lines = input.split('\n').length;
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-console.log('Characters: ', chars);
-console.log('Words: ', words);
-console.log('Lines: ', lines)
+rl.question('Please specify the filename: ', input => {
+	console.log(input);
+	fs.readFile(input, 'utf8', (err, content) => {
+		var chars = content.length;
+		var words = content.split(/[\t \n]/).filter(e => e !== '').length;
+		var lines = content.split('\n').length;
+		console.log('Characters:', chars);
+		console.log('Words: ', words);
+		console.log('Lines: ', lines);
+		process.exit();
+	});
+});
