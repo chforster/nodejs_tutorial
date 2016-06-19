6d5783a1bea3562ccd9e58dc7da11af23e576831
Christoph Forster <christoph@forster.tirol>
2016-06-19 13:08:44 +0200

    extracted router

--- chapter_09_expressjs/index.js
+++ chapter_09_expressjs/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.send('My first express application');
-});
+require('./router')(app);
 
 app.listen(8080, function() {
 	console.log("Server linstening on port 8080");
